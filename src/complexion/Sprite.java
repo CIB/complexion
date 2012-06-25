895d90c9647fccce46512d05332bb23ad80de798
cib <cib123@googlemail.com>
2012-06-24 22:46:30 -0700

    More work on the PNG metadata parser.

--- src/complexion/Sprite.java
+++ src/complexion/Sprite.java
@@ -3,6 +3,8 @@ package complexion;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.*;
+import java.util.regex.*;
+
 import com.sixlegs.png.PngImage;
 import com.sixlegs.png.TextChunk;
 
@@ -38,6 +40,12 @@ public class Sprite {
 		parseDMI(stream);
 	}
 	
+	/**
+	 * Internal function for parsing .dmi metadata and building this.states
+	 * from it.
+	 * 
+	 * @throws IOException
+	 */
 	private void parseDMI(InputStream source) throws IOException
 	{
 		// Load the PNG into memory and decode it
@@ -49,9 +57,69 @@ public class Sprite {
 		if(chunk == null) throw new IOException("File has no metadata");
 		String metadata = chunk.getText();
 		
-		// TODO: Parse the metadata and populate this.states
+		// Parse the metadata
+		List<SpriteInfoBlock> nodes = parseMetadata(metadata);
+		
+		for(SpriteInfoBlock s : nodes)
+		{
+			System.out.println(s.key + "=" + s.value);
+		}
+	}
+	
+	/**
+	 * Internal function for parsing plaintext metadata and building a kind of
+	 * abstract syntax tree from it.
+	 */
+	private List<SpriteInfoBlock> parseMetadata(String data) throws IOException
+	{
+		// Prepare a "syntax tree" to return
+		List<SpriteInfoBlock> nodes = new ArrayList<SpriteInfoBlock>();
+		
+		// Split the metadata into individual lines and parse them individually
+		String[] lines = data.split("\n");
+		
+		for(String line : lines) {
+			// If the line is all spaces, or begins with #, ignore it
+			if(line.matches("\\s*") || line.matches("#.*"))
+			{
+				continue;
+			}
+			
+			// Create a regular expression to parse lines of the type:
+			// key = value
+			Pattern pattern = Pattern.compile("(\\s*)([^\\s]+)\\s*=\\s*(.*[^\\s])\\s*");
+			Matcher matcher = pattern.matcher(line);
+			
+			// Try to match our line against the key = value pattern
+			boolean matchFound = matcher.matches();
+			if(!matchFound) throw new IOException("Malformed .DMI metadata");
+			
+			String indentation = matcher.group(1);
+			String key = matcher.group(2);
+			String value = matcher.group(3);
+			
+			SpriteInfoBlock new_block = new SpriteInfoBlock();
+			new_block.indent = (indentation.length() != 0);
+			new_block.key    = key;
+			new_block.value  = value;
+			
+			nodes.add(new_block);
+		}
+
+		return nodes;
 	}
 
 	private Map<String,List<BufferedImage>> states;
 	private String metadata;
+}
+
+/**
+ * Private class used to temporarily store the key-value pairs in
+ * that have been stored in the PNG metadata.
+ */
+class SpriteInfoBlock
+{
+	boolean indent = false; // whether this key-value pair is indented
+	String  key    = null;  // the key(left-hand side) of the pair
+	String  value  = null;  // the value(right-hand side) of the pair
 }
---
+++ src/complexion/Test.java
@@ -0,0 +1,10 @@
+package complexion;
+
+import java.io.IOException;
+
+public class Test {
+	public static void main(String[] args) throws IOException
+	{
+		Sprite s = new Sprite("mask.dmi");
+	}
+}
