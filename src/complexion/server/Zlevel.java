d078c70416aa6f18f73eec0fd2883c37abd997d6
Head <sebbebroberg@hotmail.com>
2012-06-29 00:20:27 +0200

    Fixed some mistakes regarding zlevel
    Signed-off-by: Head <sebbebroberg@hotmail.com>

--- src/complexion/server/Zlevel.java
+++ src/complexion/server/Zlevel.java
@@ -13,17 +13,17 @@ package complexion.server;
 public class Zlevel {
 	/// Fixed sized array containing chunks each being 50x50(tiles) 
 	private Chunk[][] map;
-	private int chunk_width = 50;
-	private int chunk_height = 50;
+	private int level_width = 50;
+	private int level_height = 50;
 
 	/**
 	 * Processes all chunks on the zlevel that are active.
 	 */
 	public void Tick()
 	{
-		for(int x=0;x<chunk_width;x++)
+		for(int x=0;x<level_width;x++)
 		{
-			for(int y =0;y<chunk_height;y++)
+			for(int y =0;y<level_height;y++)
 			{
 				Chunk cur_chunk = map[x][y];
 				if(cur_chunk.isActive()) // if it's not in hibernation tick it.
@@ -33,10 +33,13 @@ public class Zlevel {
 	}
 	/**
 	 * Get the tile at the specified tile-position(NOT pixel-position)
-	 * Returns null if Tile is past map boundaries.
+	 * Returns null if Tile is past map boundaries. or the chunk is not present.
 	 */
 	Tile getTile(int pos_x,int pos_y)
 	{
-		return map[pos_x / chunk_width][pos_y / chunk_height].getTile(pos_x, pos_y);
+		Chunk chunk = map[pos_x / level_width][pos_y / level_height];
+		if(chunk == null)
+			return null;
+		return chunk.getTile(pos_x, pos_y);
 	}
 }
