35c7e9aeb978e30913871b845b9515be31f5eb9f
cib <blub@blub-Latitude-D430.(none)>
2012-06-30 02:52:55 +0200

    Forgot another file.

---
+++ src/complexion/network/message/SendResourceHashes.java
@@ -0,0 +1,14 @@
+package complexion.network.message;
+
+import java.util.HashMap;
+
+/**
+ * Transfer an overview of all resource files and their CRC32 hashes.
+ * This makes it possible to find out which resources on the client are outdated.
+ * The client might send this to the server as a form of request for receiving all
+ * outdated files.
+ *
+ */
+public class SendResourceHashes {
+	public HashMap<String,Long> filenameToCRC;
+}
